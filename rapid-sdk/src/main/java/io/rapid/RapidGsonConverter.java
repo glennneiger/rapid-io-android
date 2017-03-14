fafbc4a82a8d6bc725c617479aa307928f67546f
Jakub Kinst <jakub@kinst.cz>
2017-03-14 13:53:06 +0100

    RapidJsonConverter toJson() added

--- rapid-sdk/src/main/java/io/rapid/RapidGsonConverter.java
+++ rapid-sdk/src/main/java/io/rapid/RapidGsonConverter.java
@@ -13,4 +13,10 @@ public class RapidGsonConverter implements RapidJsonConverter {
 	public <T> T fromJson(String json, Class<T> type) {
 		return mGson.fromJson(json, type);
 	}
+
+
+	@Override
+	public <T> String toJson(T object) {
+		return mGson.toJson(object);
+	}
 }
--- rapid-sdk/src/main/java/io/rapid/RapidJsonConverter.java
+++ rapid-sdk/src/main/java/io/rapid/RapidJsonConverter.java
@@ -3,4 +3,5 @@ package io.rapid;
 
 public interface RapidJsonConverter {
 	<T> T fromJson(String json, Class<T> type);
+	<T> String toJson(T object);
 }
