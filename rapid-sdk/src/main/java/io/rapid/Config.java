4e0c51413ff261c01a589812c9615db884f6535a
Leoš Dostál <dostalleos@gmail.com>
2017-04-07 11:30:17 +0200

    Config

--- rapid-sdk/src/main/java/io/rapid/Config.java
+++ rapid-sdk/src/main/java/io/rapid/Config.java
@@ -12,7 +12,8 @@ class Config {
 	public static final String URI = "ws://13.64.77.202:8080";
 	public static final int DEFAULT_LIMIT = 100;
 	public static final String ID_IDENTIFIER = "$id";
-	public static final int MESSAGE_TIMEOUT = 30 * 1000;
+	public static final long CONNECTION_TIMEOUT = 30 * DateUtils.SECOND_IN_MILLIS;
+	public static final long MESSAGE_TIMEOUT = 30 * DateUtils.SECOND_IN_MILLIS;
 	public static final long HB_PERIOD = 10 * DateUtils.SECOND_IN_MILLIS;
 	public static final String API_KEY_METADATA = "io.rapid.apikey";
 }
