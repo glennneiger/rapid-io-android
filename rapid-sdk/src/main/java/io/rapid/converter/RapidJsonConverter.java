ebd4acd3796e793bf8d4e49938ab84bb59df9d33
Jakub Kinst <jakub@kinst.cz>
2017-05-03 09:55:57 +0200

    Refactored JsonConverter

--- rapid-sdk/src/main/java/io/rapid/converter/RapidJsonConverter.java
+++ rapid-sdk/src/main/java/io/rapid/converter/RapidJsonConverter.java
@@ -4,6 +4,12 @@ package io.rapid.converter;
 import java.io.IOException;
 
 
+/**
+ * TODO
+ *
+ * (note: every implementation of this interface should serialize {@link java.util.Date} into lexicographically sortable String in order
+ * to be able to make queries based on Date properties)
+ */
 public interface RapidJsonConverter {
 	<T> T fromJson(String json, Class<T> type) throws IOException;
 	<T> String toJson(T object) throws IOException;
