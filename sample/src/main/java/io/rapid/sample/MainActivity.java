0f5f4d3b848bc00fa657733d2ef5069b39950517
Jakub Kinst <jakub@kinst.cz>
2017-05-03 15:42:41 +0200

    Added search to sample

--- sample/src/main/java/io/rapid/sample/MainActivity.java
+++ sample/src/main/java/io/rapid/sample/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.os.StrictMode;
 import android.support.graphics.drawable.VectorDrawableCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.Menu;
@@ -34,6 +36,7 @@ public class MainActivity extends AppCompatActivity implements TodoItemViewModel
 	private RapidCollectionReference<Todo> mTodos;
 	private MenuItem mToggleSubscriptionMenu;
 	private MenuItem mToggleAuthMenu;
+	private String mSearchQuery;
 
 
 	private static void log(String message) {
@@ -72,6 +75,27 @@ public class MainActivity extends AppCompatActivity implements TodoItemViewModel
 
 		mTodos = Rapid.getInstance().collection("todos_01", Todo.class);
 		subscribe();
+
+		mBinding.searchField.addTextChangedListener(new TextWatcher() {
+			@Override
+			public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+			}
+
+
+			@Override
+			public void onTextChanged(CharSequence s, int start, int before, int count) {
+				mSearchQuery = s.toString();
+				unsubscribe();
+				subscribe();
+			}
+
+
+			@Override
+			public void afterTextChanged(Editable s) {
+
+			}
+		});
 	}
 
 
@@ -189,6 +213,9 @@ public class MainActivity extends AppCompatActivity implements TodoItemViewModel
 			Drawable icon = VectorDrawableCompat.create(getResources(), R.drawable.ic_cloud_off, null);
 			mToggleSubscriptionMenu.setTitle(R.string.unsubscribe);
 			mToggleSubscriptionMenu.setIcon(icon);
+		}
+		if (mSearchQuery!=null && !mSearchQuery.isEmpty()){
+			mTodos.contains("mTitle", mSearchQuery);
 		}
 		mSubscription = mTodos
 				.orderBy("mChecked")
