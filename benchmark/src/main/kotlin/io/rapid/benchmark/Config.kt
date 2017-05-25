92296234d40fb3937e18018ce4b88a08888d2d12
Jakub Kinst <jakub@kinst.cz>
2017-05-25 15:55:48 +0200

    Removed benchmark from this repo

--- benchmark/src/main/kotlin/io/rapid/benchmark/Config.kt
+++
@@ -1,6 +0,0 @@
-package io.rapid.benchmark
-
-
-object Config {
-    const val API_KEY = "MTA0LjQwLjU4LjUwOjgwODA="
-}
--- benchmark/src/main/kotlin/io/rapid/benchmark/viewer/ViewerItemViewModel.kt
+++
@@ -1,8 +0,0 @@
-package io.rapid.benchmark.viewer
-
-import io.rapid.RapidDocument
-
-
-class ViewerItemViewModel(val item: RapidDocument<Map<String, Any>>) {
-    val items: List<String> = ArrayList<String>(item.body.values.map { it.toString() }).apply { add(0, item.id) }
-}
