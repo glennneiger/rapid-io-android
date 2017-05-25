fe75c737db68bbfd143ab197ba70ee4931304ce0
Jakub Kinst <jakub@kinst.cz>
2017-05-25 17:35:04 +0200

    Added test for ordering

--- rapid-sdk-android/src/androidTest/java/io/rapid/rapidsdk/CollectionTest.java
+++ rapid-sdk-android/src/androidTest/java/io/rapid/rapidsdk/CollectionTest.java
@@ -7,11 +7,15 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
 import java.util.UUID;
 import java.util.concurrent.CountDownLatch;
 
 import io.rapid.Rapid;
 import io.rapid.RapidCollectionReference;
+import io.rapid.RapidDocument;
 import io.rapid.rapidsdk.base.BaseRapidTest;
 
 import static org.junit.Assert.assertEquals;
@@ -33,6 +37,33 @@ public class CollectionTest extends BaseRapidTest {
 		testLimit(10, 1);
 		testLimit(10, 10);
 		testLimit(10, 20);
+	}
+
+
+	@Test
+	public void testOrder() throws InterruptedException {
+		RapidCollectionReference<Car> collection = Rapid.getInstance().collection("android_instr_test_002_" + UUID.randomUUID().toString(), Car.class);
+
+		// add n documents
+		CountDownLatch lock = new CountDownLatch(10);
+		for(int i = 0; i < 10; i++) {
+			collection.newDocument().mutate(new Car("car", new Random().nextInt()))
+					.onSuccess(lock::countDown)
+					.onError(error -> fail(error.getMessage()));
+		}
+		lock.await();
+
+		collection.orderBy("number").fetch(rapidDocuments -> {
+			ArrayList<RapidDocument<Car>> orderedList = new ArrayList<>(rapidDocuments);
+			Collections.sort(orderedList, (o1, o2) -> Integer.compare(o1.getBody().getNumber(), o2.getBody().getNumber()));
+			for(int i = 0; i < orderedList.size(); i++) {
+				assertEquals("Order does not match", rapidDocuments.get(i).getId(), orderedList.get(i).getId());
+			}
+			unlockAsync();
+		}).onError(error -> {
+			fail(error.getMessage());
+		});
+		lockAsync();
 	}
 
 
@@ -62,6 +93,4 @@ public class CollectionTest extends BaseRapidTest {
 		});
 		lockAsync();
 	}
-
-
 }
