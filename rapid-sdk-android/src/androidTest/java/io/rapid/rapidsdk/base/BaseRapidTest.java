e0f11b4f6a5221c67acd442fd7281599e75017c0
Jakub Kinst <jakub@kinst.cz>
2017-06-26 13:15:12 +0200

    Fixed dev auth token

--- rapid-sdk-android/src/androidTest/java/io/rapid/rapidsdk/base/BaseRapidTest.java
+++ rapid-sdk-android/src/androidTest/java/io/rapid/rapidsdk/base/BaseRapidTest.java
@@ -20,7 +20,7 @@ public class BaseRapidTest {
 		InstrumentationRegistry.getInstrumentation().runOnMainSync(() -> {
 			Rapid.initialize("ZGV2LXdzLXNlcnZpY2UucmFwaWQuaW8=");
 			if(authorize)
-				Rapid.getInstance().authorize("eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJydWxlcyI6W3siY2hhbm5lbCI6Ii4qIiwicmVhZCI6dHJ1ZSwid3JpdGUiOnRydWV9LHsiY29sbGVjdGlvbiI6Ii4qIiwicmVhZCI6dHJ1ZSwiY3JlYXRlIjp0cnVlLCJ1cGRhdGUiOnRydWUsImRlbGV0ZSI6dHJ1ZX1dfQ.uTfMkm_fTlEw6QaudEJrXg8G2ay8uZhYMoUdqpmlAuY");
+				Rapid.getInstance().authorize("eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJydWxlcyI6W3siY2hhbm5lbCI6eyJwYXR0ZXJuIjoiLioifSwicmVhZCI6dHJ1ZSwid3JpdGUiOnRydWV9LHsiY29sbGVjdGlvbiI6eyJwYXR0ZXJuIjoiLioifSwicmVhZCI6dHJ1ZSwiY3JlYXRlIjp0cnVlLCJ1cGRhdGUiOnRydWUsImRlbGV0ZSI6dHJ1ZX1dfQ.MdQbdW958yzRQk46qj7_bY92A60pxtSkDgy9yJV7Vd8");
 		});
 	}
 
