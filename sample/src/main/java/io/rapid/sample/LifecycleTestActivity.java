1600eb5be45b7c0fa055b0abf502be66bd6a9fb9
Jakub Kinst <jakub@kinst.cz>
2017-08-10 12:42:37 +0200

    Fixed sample

--- sample/src/main/java/io/rapid/sample/LifecycleTestActivity.java
+++ sample/src/main/java/io/rapid/sample/LifecycleTestActivity.java
@@ -8,7 +8,7 @@ import android.util.Log;
 import android.view.View;
 
 import io.rapid.Rapid;
-import io.rapid.lifecycle.RapidCollectionLiveData;
+import io.rapid.lifecycle.RapidLiveData;
 
 
 public class LifecycleTestActivity extends LifecycleActivity {
@@ -18,7 +18,7 @@ public class LifecycleTestActivity extends LifecycleActivity {
 		setContentView(new View(this));
 
 		Rapid.getInstance().authorize(Config.MASTER_AUTH_TOKEN);
-		RapidCollectionLiveData.from(Rapid.getInstance().collection("android_lifecycle_test_001")).observe(this, rapidDocuments -> {
+		RapidLiveData.from(Rapid.getInstance().collection("android_lifecycle_test_001")).observe(this, rapidDocuments -> {
 			Log.d("lifecycle", rapidDocuments.toString());
 		});
 	}
